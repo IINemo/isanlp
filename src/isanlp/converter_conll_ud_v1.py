dccbd0d5af83b06dd69cb96372f811f0fab5b749
tchewik <elenachistov@gmail.com>
2018-07-09 22:20:14 +0300

    The tasks are accomplished.

--- src/isanlp/converter_conll_ud_v1.py
+++ src/isanlp/converter_conll_ud_v1.py
@@ -1,23 +1,76 @@
-from conll_format_parser import ConllFormatStreamParser
+from .conll_format_parser import ConllFormatStreamParser
+from .annotation import WordSynt
+import logging
 
+logger = logging.getLogger('isanlp')
 
-# TODO: @elena copy all functions for annotation extraction.
-# TODO: @elena I moved parse_conll here as a useful function for dealing with conll. This function is good
-# but it creates some overhead and slower than direct indexes.
-# consider using int literals like: 
-# ID = 0 
-# FORM = 1
-# LEMMA = 2
-
-def parse_conll(self, string):
-    keys = ['id', 'form', 'lemma', 'UPOS', 'XPOS', 'feats', 'head', 'deprel', 'deps']
-    return [[dict(zip(keys, word.split('\t')[:10])) for word in sentence.split("\n")[2:-2]] for sentence in
-            string.split("sent_id = ")[1:]]
 
 class ConverterConllUDV1:
-    def __call__(conll_raw_text):
-        # TODO: @elena  
-        # input: conll_raw_text 
-        # output: all available annotations 
-        # Please, use the ConllFormatStreamParser for your purposes.
-        pass
+    FORM = 1
+    LEMMA = 2
+    POSTAG = 3
+    MORPH = 5
+    HEAD = 6
+    DEPREL = 7
+
+
+    def __call__(self, conll_raw_text):
+        """Performs conll text parsing.
+
+            Args:
+                text(str): text.
+
+            Returns:
+                Dictionary that contains:
+                1. form - list of lists with forms of words.
+                2. lemma - list of lists of strings that represent lemmas of words.
+                3. postag - list of lists of strings that represent postags of words.
+                4. morph - list of lists of strings that represent morphological features.
+                5. syntax_dep_tree - list of lists of objects WordSynt that represent a dependency tree
+            """
+
+        try:
+            result_form = list()
+            result_lemma = list()
+            result_postag = list()
+            result_morph = list()
+            result_synt = list()
+            for sent in ConllFormatStreamParser(conll_raw_text):
+                new_sent_form = list()
+                new_sent_lemma = list()
+                new_sent_postag = list()
+                new_sent_morph = list()
+                new_sent_synt = list()
+                for word in sent:
+                    if word[0][0] != '#':
+                        new_sent_form.append(word[self.FORM])
+                        new_sent_lemma.append(word[self.LEMMA])
+                        new_sent_postag.append(word[self.POSTAG])
+                        new_sent_morph.append(self._parse_morphology(word[self.MORPH]))
+                        new_sent_synt.append(self._parse_synt_tree(word[self.HEAD], word[self.DEPREL]))
+                result_form.append(new_sent_form)
+                result_lemma.append(new_sent_lemma)
+                result_postag.append(new_sent_postag)
+                result_morph.append(new_sent_form)
+                result_synt.append(new_sent_synt)
+
+        except IndexError as err:
+            logger.error('Err: Index error: {}'.format(err))
+            logger.error('--------------------------------')
+            logger.error(conll_raw_text)
+            logger.error('--------------------------------')
+            raise
+
+        return {'form': result_form,
+                'lemma': result_lemma,
+                'postag': result_postag,
+                'morph': result_morph,
+                'synt_dep_tree': result_synt}
+
+    def _parse_morphology(self, string):
+        result = [(s.split('=')) for s in string.split('|') if len(string) > 2]
+        return {feature[0]: feature[1] for feature in result}
+
+    def _parse_synt_tree(self, head, deprel):
+        return WordSynt(parent=int(head) - 1,
+                        link_name=deprel)
--- src/isanlp/processor_udpipe.py
+++ src/isanlp/processor_udpipe.py
@@ -1,120 +1,87 @@
 import sys
 from ufal.udpipe import Model, Pipeline, ProcessingError
 from . import annotation as ann
+from .converter_conll_ud_v1 import ConverterConllUDV1
 
-# TODO: @elena
-# 1) Move all annotation extraction functions to separate class converter_conll_ud_v1.py . Thats because we 
-# have the same functionality in processor_syntaxnet_remote.py. I will update it to use this class.
-# 2) Remove custom substring search function, use the standard one. 
-# 3) Document __call__ method and prettify code according pep. I see at least discrepancy in string quotes. 
 
 class ProcessorUDPipe:
+    """Wrapper around UDPipe - Trainable pipeline library.
+
+    Performs:
+    1. Tokenization.
+    2. Tagging.
+    3. Lemmatizing.
+    4. Parsing.
+    """
+
     def __init__(self, model_path):
         self.model = Model.load(model_path)
         if not self.model:
-            sys.stderr.write("Cannot load model from file '%s'\n" % model_path)
+            sys.stderr.write('Cannot load model from file "%s"\n' % model_path)
 
         self.pipeline = Pipeline(self.model, 'generic_tokenizer', Pipeline.DEFAULT, Pipeline.DEFAULT, 'conllu')
         self.error = ProcessingError()
+        self.converter_conll = ConverterConllUDV1()
 
     def __call__(self, text):
+        """Performs tokenization, tagging, lemmatizing and parsing.
+
+        Args:
+            text(str): text.
+
+        Returns:
+            Dictionary that contains:
+            1. tokens - list of objects Token.
+            2. sentences - list of objects Sentence.
+            3. lemma - list of lists of strings that represent lemmas of words.
+            4. postag - list of lists of strings that represent postags of words.
+            5. morph - list of lists of strings that represent morphological features.
+            6. syntax_dep_tree - list of lists of objects WordSynt that represent a dependency tree
+        """
         processed = self.pipeline.process(text, self.error)
         if self.error.occurred():
-            sys.stderr.write("An error occurred when calling ProcessorUDPipe: ")
+            sys.stderr.write('An error occurred when calling ProcessorUDPipe: ')
             sys.stderr.write(self.error.message)
-            return sys.stderr.write("\n")
+            return sys.stderr.write('\n')
 
-        annotation = self.parse_conll(processed)
+        annotation = self.converter_conll(processed)
 
-        return {'text': text,
-                'tokens': self.get_tokens(text, annotation),
-                'sentences': self.sentence_split(annotation),
-                'lemma': self.get_lemma(annotation),
-                'postag': self.get_postag(annotation),
-                'syntax_dep_tree': self.get_syntax_dep_tree(annotation),
-                'morph': self.get_morph(annotation)
-                }
+        annotation['sentences'] = self.sentence_split(annotation['form'])
+        annotation['tokens'] = self.get_tokens(text, annotation['form'])
 
-    # TODO: @elena move all this functions to separate class converter_conll_ud_v1.py
-    def parse_conll(self, string):
-        keys = ['id', 'form', 'lemma', 'UPOS', 'XPOS', 'feats', 'head', 'deprel', 'deps']
-        return [[dict(zip(keys, word.split('\t')[:10])) for word in sentence.split("\n")[2:-2]] for sentence in
-                string.split("sent_id = ")[1:]]
+        return annotation
 
-    def get_tokens(self, text, annotation):
+
+    def get_tokens(self, text, form_annotation):
         """Performs sentence splitting.
+
+        Args:
+            form_annotation(list): list of lists of forms of words.
 
         Returns:
             List of objects Token.
         """
 
-        # TODO: @elena at least use standard `find` function in string. Why custom? 
-        # (optional) you can also checkout fast substring search functions.
-        def get_position(text, start, form): 
-            # no way to get Token from udpipe.pipeline
-            while (form != text[start: start + len(form)] and start + len(form) <= len(text)):
-                start += 1
-            return (start, start + len(form))
-
-        ann_tokens = []
-        position = 0
-
-        for sent in annotation:
-            for word in sent:
-                form = word['form']
-                begin, end = get_position(text, position, form)
-                ann_tokens.append(ann.Token(text=word['form'], begin=begin, end=end))
-                position = end
+        ann_tokens = list()
+        for sent in form_annotation:
+            for form in sent:
+                begin = text.find(form)
+                end = begin + len(form)
+                ann_tokens.append(ann.Token(text=form, begin=begin, end=end))
         return ann_tokens
 
-    def sentence_split(self, annotation):
+    def sentence_split(self, form_annotation):
         """Performs sentence splitting.
+
+        Args:
+            form_annotation(list): list of lists of forms of words.
 
         Returns:
             List of objects Sentence.
         """
-        sentences = []
+        sentences = list()
         start = 0
-        for sent in annotation:
+        for sent in form_annotation:
             sentences.append(ann.Sentence(begin=start, end=start + len(sent)))
             start += len(sent)
         return sentences
-
-    def get_syntax_dep_tree(self, annotation):
-        """Performs syntax dep tree parsing.
-
-        Returns:
-            List of objects WordSynt for each sentence.
-        """
-        syntax_dep_tree = []
-        for sent in annotation:
-            syntax_dep_tree.append([])
-            for word in sent:
-                syntax_dep_tree[-1].append(ann.WordSynt(link_name=word['deprel'], parent=word['head']))
-        return syntax_dep_tree
-
-    def get_lemma(self, annotation):
-        """
-        Returns:
-             List of lists (sentences) of lemmas.
-        """
-        return [[word['lemma'] for word in sent] for sent in annotation]
-
-    def get_postag(self, annotation):
-        """
-        Returns:
-             List of lists (sentences) of postags.
-        """
-        return [[word['UPOS'] for word in sent] for sent in annotation]
-
-    def get_morph(self, annotation):
-        """
-        Returns:
-             List of lists (sentences) of morphological annotations.
-        """
-
-        def parse_string(morph_string):
-            tmp = [(s.split('=')) for s in morph_string.split('|') if len(morph_string) > 2]
-            return {feature[0]: feature[1] for feature in tmp}
-
-        return [[parse_string(word['feats']) for word in sent] for sent in annotation]
