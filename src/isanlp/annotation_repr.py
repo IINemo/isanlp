8d1da045c535d854a0c89656159f3de427f4f5e3
iinemo <ArtemShelmanov@gmail.com>
2018-09-02 22:01:38 +0300

    Fix CSentence

--- src/isanlp/annotation_repr.py
+++ src/isanlp/annotation_repr.py
@@ -18,9 +18,18 @@ class CSentence(Sentence):
     
     def __iter__(self):
         return (e for e in self.tokens[self.begin : self.end])
+    
+    def __str__(self):
+        return ' '.join([e.text for e in self])
         
     def __getitem__(self, idx):
-        if idx + self.begin >= self.end:
-            raise IndexError()
+        if idx >= 0:
+            if idx + self.begin >= self.end:
+                raise IndexError()
+
+            return self.tokens[self.begin + idx]
+        else:
+            if idx + self.end < self.begin:
+                raise IndexError()
             
-        return self.tokens[self.begin + idx]
+            return self.tokens[self.end + idx]
