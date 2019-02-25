0caf320bee6c692f783324fe143b07f32ac272d5
tchewik <elenachistov@gmail.com>
2019-02-25 23:39:16 +0000

    Fix with abbreviations

--- src/isanlp/en/processor_tokenizer_nltk_en.py
+++ src/isanlp/en/processor_tokenizer_nltk_en.py
@@ -1,15 +1,31 @@
 from ..annotation import Token
-from nltk.tokenize.regexp import WordPunctTokenizer
+from nltk.tokenize import RegexpTokenizer
+from ..ru.processor_tokenizer_ru import _ru_rules
 
+_en_abbrevs = [
+    r'dr',
+    r'vs',
+    r'mr\.',
+    r'mrs\.',
+    r'prof',
+    r'inc',
+    r'sc',
+    r'ph',
+    r'B\.Sc\.',
+    r'Ph\.D\.',
+    r'\w\. ?\w\.',
+]
+
+_en_regex = u'|'.join(_en_abbrevs + _ru_rules)
 
 class ProcessorTokenizerNltkEn:
     """Performs tokenization of English texts.
     
-    Simple wrapper around NLTK WordPunctTokenizer.
+    Wrapper around NLTK RegexpTokenizer.
     """
     
     def __init__(self):
-        self._nltk_proc = WordPunctTokenizer()
+        self._proc = RegexpTokenizer(_ru_regex)
     
     def __call__(self, text):
         """Performs tokenization of text.
--- src/isanlp/processor_sentence_splitter.py
+++ src/isanlp/processor_sentence_splitter.py
@@ -1,5 +1,9 @@
 from .annotation import Sentence
+from nltk.tokenize.punkt import PunktParameters
 from nltk.tokenize.punkt import PunktSentenceTokenizer
+from .ru.processor_tokenizer_ru import _ru_abbrevs
+from .en.processor_tokenizer_nltk_en import _en_abbrevs
+from itertools import combinations
 
 
 class ProcessorSentenceSplitter:
@@ -9,7 +13,9 @@ class ProcessorSentenceSplitter:
     """
     
     def __init__(self):
-        self.sent_tokeniser_ = PunktSentenceTokenizer()
+        punkt_param = PunktParameters()
+        punkt_param.abbrev_types = self.compile_abbreviations()
+        self.sent_tokeniser_ = PunktSentenceTokenizer(punkt_param)
     
     def __call__(self, tokens):
         sents = self.sent_tokeniser_.sentences_from_tokens((e.text for e in tokens))
@@ -18,5 +24,20 @@ class ProcessorSentenceSplitter:
         for sent in sents:
             res_sents.append(Sentence(curr, curr + len(sent)))
             curr += len(sent)
-        
+              
         return res_sents
+    
+    def compile_abbreviations(self):
+        def get_dot_pairs(alphabet):
+            return ['.'.join(abbrev) for abbrev in list(combinations(alphabet, 2))]
+            
+        def clean_regexps(regexps):
+            return [''.join(abbrev.lower().split('.')[:-1]).replace('\\', '').replace(u'\xad', '').replace(' ', '.').replace('?', ' ').lower() for abbrev in regexps]
+        
+        ru_abbrevs = get_dot_pairs('цукенгшзхфвапролджэячсмитбю')
+        ru_abbrevs += clean_regexps(_ru_abbrevs)
+
+        en_abbrevs = get_dot_pairs('qwertyuiopasdfghjklzxcvbnm')
+        en_abbrevs += clean_regexps(_en_abbrevs)
+        
+        return list(set(ru_abbrevs + en_abbrevs))
